52aa6889d243c6b8759f4ed6ada7bc87972c4782
mohamed <med.elkhalki0@gmail.com>
2020-09-08 22:36:22 +0200

    Setup ESLint with Airbnb coding style

--- packages/back/src/app.js
+++ packages/back/src/app.js
@@ -2,9 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({
-    foo: 'bar'
+  foo: 'bar',
 }));
 
 module.exports = app;
