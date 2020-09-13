dee54ff46a78a8915703b75b31c4abb8b42bbb14
mohamed <med.elkhalki0@gmail.com>
2020-09-13 23:09:57 +0200

    ui login and home

--- packages/front/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const paragraphElement = getByText(/Hello World!/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
